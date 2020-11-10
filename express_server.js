d8f8062f51edacff90b9350a6e0bcab01a28d586
justin-moretto <JustinMorettoMTL@gmail.com>
2020-11-10 14:47:11 +0000

    added post route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -9,6 +9,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -22,7 +25,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-//working on this one
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
